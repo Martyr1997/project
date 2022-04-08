013def605c782e3837f94afae679c2cd41e4f59e
Eugene <example@gmail.com>
2022-04-08 16:06:37 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+
+// console.log('string' * 3);
+
+
+// // const result = confirm("are you here!");
+// // console.log(result);
+
+// // const answer = prompt("вам есть 18?", "18");
+// // console.log(answer);
+
+
+// const answers = [];
+
+// answers [0] = prompt('как ваше имя?', '');
+// answers [1] = prompt('как ваша фамилия?', '');
+// answers [2] = prompt('сколько вам лет?', '');
+
+// console.log(typeof(answers));
+
+
+
+
+// const name = 'Eugene';
+
+// alert(`Привет, ${name}`/);
+// console.log(` sdfsfsdf${name}/5`);
+
